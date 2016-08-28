e4ab0894a1da2093acdfde9505f3bb7783e0fa43
regstrtn <regstrtn@gmail.com>
2016-08-28 11:29:40 +0530

    Sunday upload. UnitTest

--- unittest.c
+++ unittest.c
@@ -4,12 +4,12 @@
 
 int unittest(int nummachines, char machinenames[][100], int machineinstances[], char tasks[][100], int timereq[], int numinstances, int numtasks, char semnames[][100]) {
 	int i = 0, j = 0;
-	printf("Machines: %d Instances: %d Tasks: %d\n", nummachines,numinstances, numtasks);
+	//printf("Machines: %d Instances: %d Tasks: %d\n", nummachines,numinstances, numtasks);
 	for(i=0;i<nummachines;i++) {
-		printf("%d Machine: %s Semaphore: %s Instances: %d\n", i, machinenames[i], semnames[i], machineinstances[i]);
+		//printf("%d Machine: %s Semaphore: %s Instances: %d\n", i, machinenames[i], semnames[i], machineinstances[i]);
 	}
 	for(j=0;j<numtasks;j++) {
-		printf("Task: %s Timereq: %d\n",  tasks[j], timereq[j]); 
+		//printf("Task: %s Timereq: %d\n",  tasks[j], timereq[j]); 
 	}
 
 }
